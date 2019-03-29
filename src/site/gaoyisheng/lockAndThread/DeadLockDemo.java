2e1cefb6299c9017dbee05e77afe1cdf5e903b27
timo1160139211 <1160139211@qq.com>
2019-03-29 21:29:30 +0800

    Deadock

--- src/site/gaoyisheng/lockAndThread/DeadLockDemo.java
+++ src/site/gaoyisheng/lockAndThread/DeadLockDemo.java
@@ -4,36 +4,38 @@ public class DeadLockDemo implements Runnable{
 
     public static int flag = 1;
 
-    //
+    //static 变量是 类对象共享的
     static Object o1 = new Object();
     static Object o2 = new Object();
 
     @Override
     public void run() {
-        System.out.println("flag = " + flag);
+        System.out.println(Thread.currentThread().getName() + "：此时 flag = " + flag);
         if(flag == 1){
             synchronized (o1){
                 try {
-                    System.out.println("lock o1");
-                    Thread.sleep(750);
+                    System.out.println("我是" + Thread.currentThread().getName() + "锁住 o1");
+                    Thread.sleep(3000);
+                    System.out.println(Thread.currentThread().getName() + "醒来->准备获取 o2");
                 }catch (Exception e){
                     e.printStackTrace();
                 }
                 synchronized (o2){
-                    System.out.println("get o2");
+                    System.out.println(Thread.currentThread().getName() + "拿到 o2");
                 }
             }
         }
         if(flag == 0){
             synchronized (o2){
                 try {
-                    System.out.println("lock o2");
-                    Thread.sleep(750);
+                    System.out.println("我是" + Thread.currentThread().getName() + "锁住 o2");
+                    Thread.sleep(3000);
+                    System.out.println(Thread.currentThread().getName() + "醒来->准备获取 o2");
                 }catch (Exception e){
                     e.printStackTrace();
                 }
                 synchronized (o1){
-                    System.out.println("get o1");
+                    System.out.println(Thread.currentThread().getName() + "拿到 o1");
                 }
             }
         }
@@ -44,13 +46,17 @@ public class DeadLockDemo implements Runnable{
         DeadLockDemo t1 = new DeadLockDemo();
         DeadLockDemo t2 = new DeadLockDemo();
         t1.flag = 1;
-        t2.flag = 0;
-        //td1,td2都处于可执行状态，但JVM线程调度先执行哪个线程是不确定的。
-        //td2的run()可能在td1的run()之前运行
         new Thread(t1).start();
+
+        //让main线程休眠1秒钟,保证t2开启锁住o2.进入死锁
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t2.flag = 0;
         new Thread(t2).start();
 
     }
-
-
 }
